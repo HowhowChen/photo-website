687b9efd893025808096efd1893e08d81ef359e2
Howhowchen <debby8698@gmail.com>
2022-11-14 00:44:57 +0800

    feat: add search images

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Search = ({ search }) => {
+const Search = ({ search, setInput }) => {
+  
+  const inputHandler = event => {
+    setInput(event.target.value)
+  }
+
   return (
     <div className="search">
-      <input type="text"/>
+      <input onChange={inputHandler} type="text"/>
       <button onClick={search}>Search</button>
     </div>
   )
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from '../components/Search'
 import Picture from '../components/Picture'
 
 const Homepage = () => {
   const [input, setInput ] = useState('')
-  const searchUrl = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`
   const [data, setData] = useState(null)
   const auth = process.env.REACT_APP_PEXELS_SECRET
   const initialUrl = "https://api.pexels.com/v1/curated?page=1&per_page=15"
-  const search = async () => {
-    const dataFetch = await fetch(initialUrl, {
+  const searchUrl = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`
+
+  //  fetch data from pexels api
+  const search = async (url) => {
+    const dataFetch = await fetch(url, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
@@ -20,12 +22,16 @@ const Homepage = () => {
     setData(parsedData.photos)
   }
 
+  //  fetch data when page loads up
+  useEffect(() => {
+    search(initialUrl)
+  }, [])
+
   return (
     <div style={{minHeight: '100vh'}}>
-      <Search search={search}/>
+      <Search search={() => search(searchUrl)} setInput={setInput} />
       <div className='pictures'>
         {data && data.map((d, index) => {
-          console.log(d.src.large)
           return <Picture data={d} key={index}/>
         })}
       </div>
